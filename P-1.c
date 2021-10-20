50e3e50a75e8c9dbdb68bf219e0ba15c7b01caf7
SuryaKarthik45 <92287773+SuryaKarthik45@users.noreply.github.com>
2021-10-20 10:50:26 +0530

    Add files via upload
    Variable Declaration

---
+++ P-1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	float cgpa;
+	char initial;
+	
+	age=12;
+	cgpa=9.9;
+	initial='P';
+	
+	printf("%d\n",age);
+	printf("%.2f\n",cgpa);
+	printf("%c",initial);
+	
+}
