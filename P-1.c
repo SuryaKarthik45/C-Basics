5522b842fa87730dc6df35c26d711f71220941db
SuryaKarthik45 <92287773+SuryaKarthik45@users.noreply.github.com>
2021-10-20 10:54:22 +0530

    Add files via upload

---
+++ P-1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	float cgpa;
+	char initial;
+	
+	age=12;
+	cgpa=9.9;
+	initial='P';
+	
+	printf("age of the student is %d\n",age);
+	printf("his GPA is %.2f\n",cgpa);
+	printf("his initial is %c",initial);
+	
+}
