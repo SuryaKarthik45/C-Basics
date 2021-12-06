d535cbcad7a19c39bfcc13a679f8ce25bdcc309a
SuryaKarthik45 <92287773+SuryaKarthik45@users.noreply.github.com>
2021-12-06 10:54:52 +0530

    Add files via upload

---
+++ positive or negative.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a>0)
+	{
+		printf("%d is a positive number",a);
+	}
+	else if (a<0)
+	{
+		printf("%d is a negative number",a);
+	}
+	else
+	{
+		printf("the number is Zero");
+	}
+}
